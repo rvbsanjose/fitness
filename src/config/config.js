6ae9d7cee6d303d15a5b79bf64106fda6a4af381
Richard <richard.vanbreemen@symphony.com>
2016-03-05 21:28:27 -0800

    Add general questions for the signup process

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,70 @@
 module.exports = {
-    firebase: 'https://personaltraining.firebaseIO.com'
+    firebase: 'https://personaltraining.firebaseIO.com',
+    signup: {
+        questions: {
+            general: [
+                {
+                    type: 'input',
+                    fieldName: 'Name'
+                },
+                {
+                    type: 'radio',
+                    fieldName: 'Gender',
+                    options: [ 'Male', 'Female' ]
+                },
+                {
+                    type: 'input',
+                    fieldName: 'Address'
+                },
+                {
+                    type: 'input',
+                    fieldName: 'City'
+                },
+                {
+                    type: 'select',
+                    fieldName: 'State'
+                },
+                {
+                    type: 'input',
+                    fieldName: 'Zip Code'
+                },
+                {
+                    type: 'input',
+                    fieldName: 'Mobile'
+                },
+                {
+                    type: 'input',
+                    fieldName: 'Email'
+                }
+            ],
+            trainers: [
+                {
+                    type: 'input',
+                    fieldName: 'Years you\'ve been a trainer'
+                }
+            ],
+            palestrato: [
+                {
+                    type: 'radio',
+                    fieldName: 'Are you looking for a trainer',
+                    options: [ 'Yes', 'No' ]
+                },
+                {
+                    type: 'radio',
+                    fieldName: 'Which gender do you prefer',
+                    options: [ 'Male', 'Female' ]
+                },
+                {
+                    type: 'radio',
+                    fieldName: 'Do you belong to any gym currently',
+                    options: [ 'Yes', 'No' ]
+                },
+                {
+                    type: 'checkbox',
+                    fieldName: 'What are your fitness goals',
+                    options: [ 'Weight Loss', 'Tone Up', 'Other' ]
+                }
+            ]
+        }
+    }
 };
